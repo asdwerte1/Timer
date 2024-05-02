806542eef40658668ce1cb6e23432b3785a41308
asdwerte1 <samjmarkham@outlook.com>
2024-05-02 14:13:26 +0100

    Finalised code - Timer attributes are now private

--- Timer.py
+++ Timer.py
@@ -1,35 +1,35 @@
 class Timer:
     def __init__(self, hours = 0, minutes = 0, seconds = 0):
         # All values initialised to zero
-        self.seconds = seconds
-        self.minutes = minutes
-        self.hours = hours
+        self.__seconds = seconds
+        self.__minutes = minutes
+        self.__hours = hours
 
     def __str__(self):
-        return set_time_to_string(self.seconds, self.minutes, self.hours)
+        return set_time_to_string(self.__seconds, self.__minutes, self.__hours)
 
     def next_second(self):
-        self.seconds += 1
+        self.__seconds += 1
         #Check overflow - adjust as required
-        if self.seconds == 60:
-            self.seconds = 0
-            self.minutes += 1
-            if self.minutes == 60:
-                self.minutes = 0
-                self.hours += 1
-                if self.hours == 24:
-                    self.hours = 0
+        if self.__seconds == 60:
+            self.__seconds = 0
+            self.__minutes += 1
+            if self.__minutes == 60:
+                self.__minutes = 0
+                self.__hours += 1
+                if self.__hours == 24:
+                    self.__hours = 0
 
     def prev_second(self):
-        self.seconds -= 1
-        if self.seconds == -1:
-            self.seconds = 59
-            self.minutes -= 1
-            if self.minutes == -1:
-                self.minutes = 59
-                self.hours -= 1
-                if self.hours == -1:
-                    self.hours = 23
+        self.__seconds -= 1
+        if self.__seconds == -1:
+            self.__seconds = 59
+            self.__minutes -= 1
+            if self.__minutes == -1:
+                self.__minutes = 59
+                self.__hours -= 1
+                if self.__hours == -1:
+                    self.__hours = 23
 
 def set_time_to_string(seconds, minutes, hours):
     """Function to read in three integer values and convert to a string in the form 
