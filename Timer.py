d5942d6f55f04f30bb8151ac442c9a5814d39ed3
asdwerte1 <samjmarkham@outlook.com>
2024-05-02 13:37:30 +0100

    Initial state of challenge

---
+++ Timer.py
@@ -0,0 +1,28 @@
+class Timer:
+    def __init__( ??? ):
+        #
+        # Write code here
+        #
+
+    def __str__(self):
+        #
+        # Write code here
+        #
+
+    def next_second(self):
+        #
+        # Write code here
+        #
+
+    def prev_second(self):
+        #
+        # Write code here
+        #
+
+
+timer = Timer(23, 59, 59)
+print(timer)
+timer.next_second()
+print(timer)
+timer.prev_second()
+print(timer)
